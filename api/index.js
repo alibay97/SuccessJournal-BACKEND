01c73a81c33fefbf1b2da7a27cd294a421d064bc
Alison <alison.bayona@gmail.com>
2023-06-08 12:01:51 -0400

    backend

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import connectToMongoDB from './controllers/connection.js';
-import { seedQuotes } from './seed/quote-seed.js';
-import { seedEntries } from './seed/entry-seed.js';
-import router from './routes/router.js';
+import connectToMongoDB from '../controllers/connection.js';
+import { seedQuotes } from '../seed/quote-seed.js';
+import { seedEntries } from '../seed/entry-seed.js';
+import router from '../routes/router.js';
 
 dotenv.config();
 const app = express();
